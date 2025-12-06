079416bfd3a10549861b962a9792523943f504cb
Tahsen Islam Sajon <sajon.tahsen@gmail.com>
2025-12-06 17:48:54 -0600

    fix for repo rename

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/CS424-assignment-4-dashboard/',
+  base: '/chi-food-inspections-embeddings-dashboard/',
 })
